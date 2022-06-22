1af4b055626a6197022b30a3ec99d7bb04de69a5
srabonti213 <107984432+srabonti213@users.noreply.github.com>
2022-06-22 13:22:30 +0600

    Add files via upload

---
+++ main_flower.js
@@ -0,0 +1,17 @@
+$('.hdd').hide()
+$('.personal-responsive-item').hide()
+$('.shh').click(function(){
+	$('.personal-responsive-item').slideDown()
+	$('.shh').hide()
+	$('.hdd').show()
+})
+$('.hdd').click(function(){
+	$('.personal-responsive-item').slideUp()
+	$('.hdd').hide()
+	$('.shh').show()
+})
+$('.click-menu').click(function(){
+	$('.personal-responsive-item').hide()
+	$('.hdd').hide()
+	$('.shh').show()
+})
